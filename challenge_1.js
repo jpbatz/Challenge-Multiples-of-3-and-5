1f190b8fb8ce3cfa94ff8c9798a700ebaf174fd6
jph <jph@hawaii.edu>
2014-10-04 10:02:41 -1000

    solved

--- challenge_1.js
+++ challenge_1.js
@@ -2,5 +2,16 @@ exports.sumOfAMultiple = function(n){
   var sum = 0;
   // do your work here
   
+  // loop through 1 (not 0) through n argument
+  for(var i=1; i<n; i++) {
+    // test for multiple of 3 or 5
+    if(i % 3 === 0 || i % 5 === 0) {
+    // if true, add to sum
+      sum += i;
+    }
+  }
+  // display sum
+  console.log(sum);
+  // return sum
   return sum;
 };
